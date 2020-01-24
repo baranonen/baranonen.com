c59419ef0798f5a49ab3cd76a9a9f428c425b366
Baran Önen <baranonenn@gmail.com>
2020-01-24 19:13:19 +0300

    Push

--- service-worker.js
+++ service-worker.js
@@ -10,3 +10,28 @@ evt.waitUntil(
       return cache.addAll(FILES_TO_CACHE);
     })
 );
+
+evt.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key !== CACHE_NAME) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+);
+
+if (evt.request.mode !== 'navigate') {
+    // Not a page navigation, bail.
+    return;
+  }
+  evt.respondWith(
+      fetch(evt.request)
+          .catch(() => {
+            return caches.open(CACHE_NAME)
+                .then((cache) => {
+                  return cache.match('index.html');
+                });
+          })
+  );
