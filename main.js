2efe8860a43e7b49423a3fa8112831d2ffa08a64
Baran Önen <baran@baranonen.com>
2024-02-25 02:03:51 +0100

    Animation flicker fixed

--- main.js
+++ main.js
@@ -1,32 +1,17 @@
 const backgrounds = [
-    'frames/frame2.svg', 'frames/frame3.svg', 'frames/frame4.svg', 'frames/frame5.svg', 'frames/frame6.svg', 'frames/frame7.svg', 'frames/frame8.svg', 'frames/frame9.svg', 'frames/frame10.svg', 'frames/frame11.svg', 'frames/frame12.svg', 'frames/frame13.svg', 'frames/frame14.svg', 'frames/frame15.svg', 'frames/frame16.svg', 'frames/frame17.svg'
-];
-
-function preloadImages(urls) {
-    return Promise.all(urls.map(url => {
-        return new Promise((resolve, reject) => {
-            const img = new Image();
-            img.src = url;
-            img.onload = resolve;
-            img.onerror = reject;
-        });
-    }));
-}
+    'frame1', 'frame2', 'frame3', 'frame4', 'frame5', 'frame6', 'frame7', 'frame8', 'frame9', 'frame10', 'frame11', 'frame12', 'frame13', 'frame14', 'frame15', 'frame16'
+]
 
 function changeBackground() {
-    let index = 0;
+    let index = 0
     const intervalId = setInterval(() => {
         if (index >= backgrounds.length) {
-            clearInterval(intervalId);
+            clearInterval(intervalId)
         } else {
-            document.getElementsByClassName("container")[0].style.backgroundImage = `url(${backgrounds[index]})`;
-            index++;
+            document.getElementById(backgrounds[index]).style.display = "none"
+            index++
         }
-    }, 3000);
+    }, 3000)
 }
 
-preloadImages(backgrounds).then(() => {
-    changeBackground();
-}).catch(error => {
-    console.error('Error preloading images:', error);
-});
+changeBackground()
